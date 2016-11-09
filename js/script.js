40eba7d03ce7f577111bb3b558b69e1fb12fbd69
Yaroslav Liakh <liakh_ya@mail.ru>
2016-11-09 21:43:02 +0200

    ready first review

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Ярик on 09.11.2016.
+ */
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
