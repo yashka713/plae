c921053661d601fba5b032072a4b06513b6e842b
Yaroslav Liakh <liakh_ya@mail.ru>
2016-11-10 15:37:46 +0200

    ready slider

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /**
  * Created by Ярик on 09.11.2016.
  */
+"use strict";
 $(document).ready(function(){
     $("#menu").on("click","a", function (event) {
         //отменяем стандартную обработку нажатия по ссылке
@@ -13,3 +14,43 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1500);
     });
 });
+/*slider*/
+var sliderFirst = {
+    slides:[
+        {img:'img/shoes/Ty_N_Steel.jpg', title: 'TY', w:446,h:237},
+        {img:'img/shoes/Max_N_Steel_Camo.jpg', title: 'MAX', w:474,h:274},
+        {img:'img/shoes/Emme_S_Pink.jpg', title: 'EMME', w:465,h:237},
+        {img:'img/shoes/Camile_Navy_3Quarter.jpg', title: 'CAMILLE', w:452,h:452},
+        {img:'img/shoes/Noel_Bracken.jpg', title: 'NOEL', w:452,h:361},
+        {img:'img/shoes/Roan_L_White.jpg', title: 'ROAN', w:459,h:235},
+        {img:'img/shoes/Nat_Navy.jpg', title: 'Nat', w:457,h:233}],
+    frame: 0, // текущий кадр для отбражения - индекс картинки из массива
+    set: function(obj) { // установка нужного фона слайдеру
+        var slide = document.getElementById("sliderFirst__img");
+        if(obj.title == "CAMILLE"){
+            document.getElementById("sliderFirst").style.marginTop = 0+"px";
+            document.getElementById("sliderFirst__left-button").style.top = 253+"px";
+            document.getElementById("sliderFirst__right-button").style.top = 253+"px";
+        }else{
+            document.getElementById("sliderFirst").style.marginTop = 100+"px";
+            document.getElementById("sliderFirst__left-button").style.top = 153+"px";
+            document.getElementById("sliderFirst__right-button").style.top = 153+"px";
+        }
+        slide.style.backgroundImage = "url("+obj.img+")";
+        slide.style.width = obj.w+"px";
+        slide.style.height = obj.h+"px";
+        
+    },
+    left: function() { // крутим на один кадр влево
+        console.log('крутим на один кадр влево');
+        this.frame--;
+        if(this.frame < 0) this.frame = this.slides.length-1;
+        this.set(this.slides[this.frame]);
+    },
+    right: function() { // крутим на один кадр вправо
+        console.log('крутим на один кадр вправо');
+        this.frame++;
+        if(this.frame == this.slides.length) this.frame = 0;
+        this.set(this.slides[this.frame]);
+    }
+};
